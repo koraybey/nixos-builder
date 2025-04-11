620751256745d95ff6c1b13816013e40bd82ea2a
koraybey <21336342+koraybey@users.noreply.github.com>
2025-04-11 15:23:36 +0200

    configure remote builder for utm vm

---
+++ README.md
@@ -0,0 +1,57 @@
+Obtain the minimal ISO image required to boot and install on UTM:https://nixos.org/download/#minimal-iso-image
+
+Create partitions
+
+```shell
+cfdisk /dev/vda
+# Choose gpt then create 3 partitions
+# 1) EFI system for boot, 1G
+# 2) Swap partition, 5G
+# 3) Linux filesystem, 16G
+# Write and quit
+```
+
+Create and mount filesystems
+
+```shell
+# use /dev/vda for the ARM64 machine, and /dev/sda for the x86_64 machine
+mkfs.fat -F 32 -n boot /dev/vda1
+mkswap -L swap /dev/vda2
+mkfs.ext4 -L nixos /dev/vda3
+# mount
+mount /dev/vda3 /mnt
+mkdir -p /mnt/boot
+mount /dev/vda1 /mnt/boot
+# enable swap
+swapon /dev/vda2
+```
+
+Move nebula certs and make them readable: https://nixos.wiki/wiki/Nebula
+
+```shell
+# Move certs to /etc/nebula, then
+sudo chmod --reference /etc/nix /etc/nebula
+sudo chmod --reference /etc/nix/nix.conf /etc/nebula/*
+```
+
+Clone configuration and install
+
+```shell
+curl -LO https://github.com/koraybey/nixos-builder
+unzip ./main.zip
+cd nixos-main && nixos-install --root /mnt --flake .#builder-aarch64
+# shutdown
+shutdown now
+```
+
+Remove the image and boot the UTM VM!
+
+Generate the key on the client so that we can authorize it on the build machine
+```shell
+ssh-keygen
+eval "$(ssh-agent -s)"
+ssh-add /home/admin/.ssh/id_ed25519
+# Obtain the public key
+cat /home/admin/.ssh/.ssh/id_ed25519.pub
+# Add it to authorizedKeys
+```
---
+++ systems/utm/builder/config.nix
@@ -0,0 +1,130 @@
+{
+  pkgs,
+  rootSSHKey,
+  userSSHKey,
+  ...
+}: {
+  networking.hostName = "builder";
+
+  nix.settings = {
+    experimental-features = "nix-command flakes";
+    auto-optimise-store = true;
+  };
+
+  environment.systemPackages = [
+    pkgs.vim
+    pkgs.git
+    pkgs.zip
+    pkgs.unzip
+    pkgs.wget
+  ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "vfat";
+  };
+  swapDevices = [
+    {
+      device = "/dev/disk/by-label/swap";
+    }
+  ];
+
+  services.spice-vdagentd.enable = true;
+
+  services.nebula.networks.mesh = {
+    enable = true;
+    lighthouses = ["192.168.100.100"];
+    relays = ["192.168.100.100"];
+    staticHostMap = {
+      "192.168.100.100" = ["nebula.medkai.de:4242"];
+    };
+    cert = "/etc/nebula/builder.crt";
+    key = "/etc/nebula/builder.key";
+    ca = "/etc/nebula/ca.crt";
+    firewall = {
+      inbound = [
+        {
+          port = "any";
+          proto = "any";
+          host = "any";
+        }
+      ];
+      outbound = [
+        {
+          port = "any";
+          proto = "any";
+          host = "any";
+        }
+      ];
+    };
+    settings = {
+      punchy = {
+        punch = true;
+        respond = true;
+      };
+    };
+  };
+
+  documentation.nixos.enable = false;
+  time.timeZone = "Europe/Berlin";
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console.keyMap = "us";
+  nix.settings.trusted-users = ["user" "@wheel"];
+  nix.settings.system-features = ["kvm" "nixos-test"];
+
+  boot = {
+    tmp.cleanOnBoot = true;
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      systemd-boot = {
+        enable = true;
+      };
+    };
+    kernelModules = ["kvm-amd" "kvm-intel"];
+  };
+  virtualisation.libvirtd.enable = true;
+
+  users.users = {
+    root = {
+      hashedPassword = "!"; # Disable root login
+      openssh.authorizedKeys.keys = [
+        rootSSHKey
+      ];
+    };
+    user = {
+      isNormalUser = true;
+      extraGroups = ["wheel"];
+      openssh.authorizedKeys.keys = [
+        userSSHKey
+      ];
+    };
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PubkeyAuthentication = true;
+    };
+  };
+  networking.firewall.allowedTCPPorts = [22];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
